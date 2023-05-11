7588e00423ffea19c68b7b15871aaf04327a8bdd
Victor Alvarez <victor.alvarez@mtrade.mx>
2023-05-11 08:37:22 -0600

    quitar poppins

--- src/components/Banner/components/Text.jsx
+++ src/components/Banner/components/Text.jsx
@@ -1,7 +1,7 @@
 const Text = () => {
   return (
     <div className="flex flex-col space-y-[16px]">
-      <span className="text-base text-primary font-semibold md:text-base-md font-poppins">AHORRA TIEMPO Y DINERO</span>
+      <span className="text-base text-primary font-semibold md:text-base-md">AHORRA TIEMPO Y DINERO</span>
       <span className="text-xl md:text-3xl font-poppins text-text-primary">
         Toda tu lista de material eléctrico, en solo lugar. <br /><span className="font-bold font-poppins">¡Al mejor precio!</span>
       </span>
