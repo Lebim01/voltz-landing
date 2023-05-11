4c578990e92cccca39a159cb5c0da21519533997
Victor Alvarez <victor.alvarez@mtrade.mx>
2023-05-11 11:28:31 -0600

    fix footer

--- src/components/Footer/components/Voltz/index.jsx
+++ src/components/Footer/components/Voltz/index.jsx
@@ -2,7 +2,7 @@ const Voltz = () => {
   return (
     <div className="mx-[16px] px-[16px] py-[24px] flex flex-col space-y-[24px] border-[0px] border-b md:border-b-[0px] border-border-1 border-solid items-start md:justify-end lg:justify-start md:flex-1">
       <img src="/voltz-logo.svg" className="h-[32px] w-min" alt="voltz-logo" />
-      <span className="font-semibold text-base text-text-primary font-poppins lg:text-center">
+      <span className="text-base text-text-primary font-poppins">
         Plataforma de compras de material eléctrico e iluminación.
       </span>
     </div>
