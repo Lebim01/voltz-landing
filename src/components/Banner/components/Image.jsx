d272080bbc1857dc42450d0c1afb34ccb318bffe
Victor Alvarez <victor.alvarez@mtrade.mx>
2023-05-12 11:30:15 -0600

    lottie

--- src/components/Banner/components/Image.jsx
+++ src/components/Banner/components/Image.jsx
@@ -1,9 +1,25 @@
+import Lottie from "react-lottie";
+import animationData from "../../../animations/banner.json";
+
+console.log(animationData);
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+};
+
 const BannerImage = () => {
   return (
     <div className="items-center justify-center w-full flex-1 hidden md:flex">
-      <img src="/banner-image.webp" alt="Banner image" className="w-[80%] md:w-[100%] max-w-[560px]" />
+      <Lottie
+        options={defaultOptions}
+        height={560}
+        width={560}
+        className="w-[80%] md:w-[100%] max-w-[560px]"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default BannerImage
+export default BannerImage;
