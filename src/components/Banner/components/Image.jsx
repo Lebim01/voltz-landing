290703c5a71fd82a38805f0287e6b868f055cb96
Victor Alvarez <victor.alvarez@mtrade.mx>
2023-05-12 11:37:12 -0600

    remove lottie

--- src/components/Banner/components/Image.jsx
+++ src/components/Banner/components/Image.jsx
@@ -1,23 +1,13 @@
-import Lottie from "react-lottie";
-import animationData from "../../../animations/banner.json";
-
-console.log(animationData);
-
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: animationData,
-};
-
 const BannerImage = () => {
   return (
     <div className="items-center justify-center w-full flex-1 hidden md:flex">
-      <Lottie
-        options={defaultOptions}
-        height={560}
-        width={560}
+      <lottie-player
+        autoplay
+        loop
+        mode="normal"
+        src="/banner.json"
         className="w-[80%] md:w-[100%] max-w-[560px]"
-      />
+      ></lottie-player>
     </div>
   );
 };
